e9abe7e08a8c2f32a4cfc2fe69fc1a21fc94ec92
oy5 <oy5@duke.edu>
2018-01-19 11:44:59 -0500

    v0.2

--- my_malloc/my_malloc/my_malloc.c
+++ my_malloc/my_malloc/my_malloc.c
@@ -4,54 +4,69 @@
 struct Node_t {
   size_t start_address;
   size_t end_address;
-  size_t size;
-  * struct Node_t next;
+  //  size_t size;
+  struct Node_t * next;
 };
 typedef struct Node_t Node;
 //global variable to store the freed memory list
 //Node * freedListHead;
 Node * allocatedListHead;
-size_t heapTop;
-/* freedListHead.next = NULL; */
-/* allocatedListHead.next = NULL; */
+void * heapTop;
 
+void addToLinkedList(Node * prev, Node * toBeAdded) {
+  Node * temp = prev->next;
+  prev->next = toBeAdded;
+  toBeAdded->next = temp;
+}
+
+void deleteFromLinkedList(Node * prev, Node * toBeDeleted) {
+  prev->next = toBeDeleted->next;
+}
+
+void initNode(Node * node, Node * prev, size_t size) {
+  node->start_address = (size_t)(node - 1);
+  node->end_address = node->start_address - size;
+  addToLinkedList(prev, node);
+}
 
 //First Fit malloc/free
 void *ff_malloc(size_t size) {
-  
-  Node ** curr = &allocatedListHead;
-  while (*curr->next != NULL) {
-    if (*curr->next->next != NULL && (*curr->next->next->end_address - *curr->next) >= size) {
-      Node * temp = *curr->next->next->end_address - 1;
-      temp->start_address = temp - 1;
-      temp->end_address = start_address - size;
-      addToLinkedList(*curr, toBeMalloc);
+  Node * curr = allocatedListHead->next; 
+  Node * prev = allocatedListHead;
+  Node * temp = NULL;
+  while (curr != NULL) {
+    if ((curr->end_address - (size_t)prev) >= size + sizeof(*curr)) {
+      temp = (Node *)(curr->end_address - sizeof(*curr));
+      break;
     }
-/*     if ((*curr)->next->size >= size + sizeof(*Node)) { */
-/*       Node * toBeMalloc = (*curr)->next; */
-/*       if (toBeMalloc->size == size) { */
-/* 	Node * temp = toBeMalloc->next; */
-/* 	addToLinkedList(allocatedListHead, toBeMalloc, 0); */
-/* 	(*curr)->next = temp; */
-/*       } else { */
-/* 	Node * newFreeNode = ff_malloc(sizeof(*Node)); */
-/* 	newFreeNode->size = mallocAddr->size - size; */
-/* 	(*curr)->next->size -= size; */
-/*       } */
-/*       return (void *)toBeMalloc->start_address; */
-/*     } */
-    curr = &(*curr->next);
+    prev = curr;
+    curr = curr->next;      
   }
-  Node * temp = sbrk(sizeof(*temp));
-  temp->size = sizeof(*temp);
-  addToLinkedList(allocatedListHead, temp, size, 0);  
-  temp = sbrk(size);
-  temp->size = size;
-  addToLinkedList(allocatedListHead, temp, size, 0);
-  return NULL;
+  if (temp == NULL) {
+    if (((size_t)heapTop - (size_t)prev) >= size + sizeof(*curr)) {
+      temp = heapTop;
+    } else {
+      temp = sbrk(size + sizeof(*curr) - (size_t)heapTop + (size_t)prev);
+      heapTop = temp;
+    }
+  }
+  initNode(temp, prev, size);
+  return (void *)(temp->start_address);
 }
+
 void ff_free(void *ptr) {
+  Node * curr = allocatedListHead->next; 
+  Node * prev = allocatedListHead;
+  while (curr != NULL) {
+    if (curr->start_address == (size_t)ptr) {
+      deleteFromLinkedList(prev, curr);
+      break;
+    }
+    prev = curr;
+    curr = curr->next;
+  }
 }
+
 //Best Fit malloc/free
 void *bf_malloc(size_t size) {
   return NULL;
